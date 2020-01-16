9f32122b30f71c264adf5d5cf5f6e70fcaf5ac3c
MariCarmenSaldivar <maricarmen.saldivar@gmail.com>
2020-01-16 15:43:27 -0500

    modificacion capabilities protractor

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -14,7 +14,7 @@ exports.config = {
   ],
   capabilities: {
     chromeOptions: {
-      args: [ "--headless" ]
+      args: [ "--headless", "--disable-gpu", "--window-size=800x600" ]
     },
     browserName: 'chrome'
   },
